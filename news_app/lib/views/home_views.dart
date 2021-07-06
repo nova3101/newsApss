6cffd26f41cfb1a3672dfad1ce0c18bead262bd7
nova3101 <novacantikafitri@gmail.com>
2021-07-06 10:44:53 +0700

    runnning

--- news_app/lib/views/home_views.dart
+++ news_app/lib/views/home_views.dart
@@ -21,7 +21,7 @@ class HomeView extends StatelessWidget {
                   style: kNonActiveTabStyle,
                 ),
                 subtitle: Text(
-                  "Jessica Veranda",
+                  "Nova Cantika fitri",
                   textAlign: TextAlign.end,
                   style: kActiveTabStyle,
                 ),
@@ -31,7 +31,7 @@ class HomeView extends StatelessWidget {
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10.0),
                       image: DecorationImage(
-                        image: AssetImage("assets/ve.jpg"),
+                        image: AssetImage("assets/Nova.jpeg"),
                         fit: BoxFit.cover,
                       )),
                 ),
